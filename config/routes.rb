5cba5e3f7b1f7cd75b957e5a5de9c450fbf4875b
aomikan10 <mm1021201@gmail.com>
2020-07-30 00:48:12 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "posts#index"
   resources :users, only: [:edit, :update]
+  resources :posts, only: [:index]
 end
