885e9cbc998372633ee60955328e180a14c54737
aomikan10 <mm1021201@gmail.com>
2020-07-30 18:54:12 +0900

    Update.db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_084446) do
+ActiveRecord::Schema.define(version: 2020_07_29_162518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_07_29_084446) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "copy"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
