2f9fde0f74168eab513815d032a104097a3275cb
aomikan10 <mm1021201@gmail.com>
2020-09-19 14:42:44 +0900

    Delete.update

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, :text, :copy, :user_id, presence: true
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   acts_as_taggable
 
